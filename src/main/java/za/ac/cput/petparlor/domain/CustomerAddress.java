fe9f596c36ecb752c9cb627be73a98c6c803265c
Onodwa2001 <onodwasiyotula7@gmail.com>
2023-04-04 15:00:54 +0200

    first commit

---
+++ src/main/java/za/ac/cput/petparlor/domain/CustomerAddress.java
@@ -0,0 +1,48 @@
+package za.ac.cput.petparlor.domain;
+
+public class CustomerAddress {
+
+    private int customerID, addressID;
+
+    public CustomerAddress() {}
+
+    public CustomerAddress(Builder builder) {
+        this.customerID = builder.customerID;
+        this.addressID = builder.addressID;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public int getAddressID() {
+        return addressID;
+    }
+
+    public static class Builder {
+
+        private int customerID, addressID;
+
+        public Builder setCustomerID(int customerID) {
+            this.customerID = customerID;
+            return this;
+        }
+
+        public Builder setAddressID(int addressID) {
+            this.addressID = addressID;
+            return this;
+        }
+
+        public Builder copy(CustomerAddress customerAddress) {
+            this.addressID = customerAddress.addressID;
+            this.customerID = customerAddress.customerID;
+            return this;
+        }
+
+        public CustomerAddress build() {
+            return new CustomerAddress(this);
+        }
+
+    }
+
+}
