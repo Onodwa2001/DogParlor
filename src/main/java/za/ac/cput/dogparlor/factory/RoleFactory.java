1d7a6d82d089f6188e694fa8b87766e80ee2d1f7
LindiweTSomana <218076509@mycput.ac.za>
2023-04-06 22:31:25 +0100

    Created Factories and tested entites

---
+++ src/main/java/za/ac/cput/dogparlor/factory/RoleFactory.java
@@ -0,0 +1,24 @@
+/*  RoleFactory.java
+    Factory for the  role
+    Author: Lindiwe Thokozile Somana (218076509)
+    Date: 5 April 2023
+*/
+package za.ac.cput.dogparlor.factory;
+
+
+import za.ac.cput.dogparlor.domain.Role;
+import za.ac.cput.dogparlor.util.Helper;
+
+public class RoleFactory {
+
+       public static Role createRole(int id, String name){
+
+        if(id == 0)
+            return  null;
+
+        if(Helper.isNullOrEmpty(name))
+            return null;
+
+        return new Role.Builder().setIdRole(id).setRoleName(name).build();
+    }
+}
